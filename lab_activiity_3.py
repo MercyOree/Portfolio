7aaf8b5d1963e3eacaaab42a660f7a07bb5cab87
MercyOree <115586010+MercyOree@users.noreply.github.com>
2022-10-23 22:34:15 -0500

    Add files via upload

---
+++ lab_activiity_3.py
@@ -0,0 +1,6 @@
+# Mercy Oreoluwa koku
+# Oct/18/2022
+# Program helps communicate with users by printing a "hello world"
+
+print("Hello world")
+
---
+++ program_2.py
@@ -0,0 +1,5 @@
+# Mercy Oreoluwa koku
+# Oct/18/2022
+# Program helps communicate with users, Asks the user for their name and greets them with their name.
+name = input('Enter you name: ')
+print("Hello", name)
